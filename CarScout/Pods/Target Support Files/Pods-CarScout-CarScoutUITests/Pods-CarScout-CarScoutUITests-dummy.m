c25801966b55b6e19c0458fd86098b356da02c97
Valentina Henao <valen410@gmail.com>
2019-11-11 11:39:24 +0100

    Install drawer-view dependency

---
+++ CarScout/Pods/Target Support Files/Pods-CarScout-CarScoutUITests/Pods-CarScout-CarScoutUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CarScout_CarScoutUITests : NSObject
+@end
+@implementation PodsDummy_Pods_CarScout_CarScoutUITests
+@end
---
+++ CarScout/Pods/Target Support Files/Pods-CarScout/Pods-CarScout-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CarScout : NSObject
+@end
+@implementation PodsDummy_Pods_CarScout
+@end

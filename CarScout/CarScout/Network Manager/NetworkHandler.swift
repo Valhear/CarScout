3bfa8a84732e8f7b6a938ac1aeed6e18b8e0e9c9
Valentina Henao <valen410@gmail.com>
2019-11-12 11:38:00 +0100

    perform url session task and check responses

--- CarScout/CarScout/Network Manager/NetworkHandler.swift
+++ CarScout/CarScout/Network Manager/NetworkHandler.swift
@@ -9,13 +9,64 @@
 import Foundation
 
 class NetworkHandler {
+    var cars = [Car]()
+    
     func getCarList() -> [Car] {
-        var cars = [Car]()
         
         //TODO: Network Request
+        let session = URLSession.shared
+        guard let url = URL(string: "https://cdn.sixt.io/codingtask/cars") else { return cars }
+
+        let task = session.dataTask(with: url) { (data, response, error) in
+            print(data)
+            print(response)
+            print(error)
+            
+            
+            //TODO: Implement Error Handling for all this cases
+            
+            //Check for errors in response
+            if error != nil {
+                //Throw an error with throw and use promises to deal with any thrown or passed errors in the chain’s .catch clause
+                //self.handleClientError(error)
+                return
+            }
+            
+            //Check for HTTP response code if errors
+            guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+                //self.handleServerError(response)
+                return
+            }
+            
+            guard let mime = response?.mimeType, mime == "application/json" else {
+                //TODO: Recover from the error
+                print("Wrong MIME type!")
+                return
+            }
+            
+            if let json = try? JSONSerialization.jsonObject(with: data!, options: []) {
+                print(json)
+            }
+        }
+        
+        task.resume()
+        
+        
+        //Get json from Local json file
         guard let filePath = Bundle.main.url(forResource: "carList", withExtension: "json") else { return cars }
         guard let dataResponse = try? Data(contentsOf: filePath) else { return cars }
         
+        decode(dataResponse: dataResponse)
+        
+        
+        //serverUrl: https://cdn.sixt.io/codingtask/cars
+        
+        
+        
+        return cars
+    }
+    
+    func decode(dataResponse: Data) {
         //Decode JSON Response Data
         do {
             let decoder = JSONDecoder()
@@ -24,7 +75,5 @@ class NetworkHandler {
         } catch let parsingError {
             print("Error", parsingError)
         }
-        
-        return cars
     }
 }
